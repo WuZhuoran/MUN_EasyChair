b05aa6593c8097b92cf4767c2b1b95ef31131eda
Oliver <work.serv.oliver@gmail.com>
2014-11-05 15:46:22 +0800

    First Update

---
+++ MUN_ChairTools/MUN_ChairTools/MainForm.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace MUN_ChairTools
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.menuStripMain = new System.Windows.Forms.MenuStrip();
+            this.SystemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ActionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OtherToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.HelpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.groupBoxInfo = new System.Windows.Forms.GroupBox();
+            this.groupBoxTime = new System.Windows.Forms.GroupBox();
+            this.buttonSetTimeReset = new System.Windows.Forms.Button();
+            this.labelColon = new System.Windows.Forms.Label();
+            this.labelSetTimeSecond = new System.Windows.Forms.Label();
+            this.labelSetTimeMinute = new System.Windows.Forms.Label();
+            this.buttonSetTimePause = new System.Windows.Forms.Button();
+            this.buttonSetTimeStart = new System.Windows.Forms.Button();
+            this.LabelSecond = new System.Windows.Forms.Label();
+            this.numericUpDownSetTime = new System.Windows.Forms.NumericUpDown();
+            this.labelSetTime = new System.Windows.Forms.Label();
+            this.labelSystemTime = new System.Windows.Forms.Label();
+            this.groupBoxRecord = new System.Windows.Forms.GroupBox();
+            this.timerSystemTime = new System.Windows.Forms.Timer(this.components);
+            this.timerSetTime = new System.Windows.Forms.Timer(this.components);
+            this.AboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStripMain.SuspendLayout();
+            this.panelMain.SuspendLayout();
+            this.groupBoxTime.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSetTime)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStripMain
+            // 
+            this.menuStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.SystemToolStripMenuItem,
+            this.ActionToolStripMenuItem,
+            this.OtherToolStripMenuItem,
+            this.HelpToolStripMenuItem});
+            this.menuStripMain.Location = new System.Drawing.Point(0, 0);
+            this.menuStripMain.Name = "menuStripMain";
+            this.menuStripMain.Size = new System.Drawing.Size(1008, 25);
+            this.menuStripMain.TabIndex = 3;
+            this.menuStripMain.Text = "menuStrip1";
+            // 
+            // SystemToolStripMenuItem
+            // 
+            this.SystemToolStripMenuItem.Name = "SystemToolStripMenuItem";
+            this.SystemToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.SystemToolStripMenuItem.Text = "文件";
+            // 
+            // ActionToolStripMenuItem
+            // 
+            this.ActionToolStripMenuItem.Name = "ActionToolStripMenuItem";
+            this.ActionToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.ActionToolStripMenuItem.Text = "操作";
+            // 
+            // OtherToolStripMenuItem
+            // 
+            this.OtherToolStripMenuItem.Name = "OtherToolStripMenuItem";
+            this.OtherToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.OtherToolStripMenuItem.Text = "其他";
+            // 
+            // HelpToolStripMenuItem
+            // 
+            this.HelpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AboutToolStripMenuItem});
+            this.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem";
+            this.HelpToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.HelpToolStripMenuItem.Text = "帮助";
+            // 
+            // panelMain
+            // 
+            this.panelMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelMain.BackColor = System.Drawing.SystemColors.Control;
+            this.panelMain.Controls.Add(this.groupBoxInfo);
+            this.panelMain.Controls.Add(this.groupBoxTime);
+            this.panelMain.Controls.Add(this.groupBoxRecord);
+            this.panelMain.Location = new System.Drawing.Point(12, 28);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(984, 690);
+            this.panelMain.TabIndex = 4;
+            this.panelMain.SizeChanged += new System.EventHandler(this.panelMain_SizeChanged);
+            // 
+            // groupBoxInfo
+            // 
+            this.groupBoxInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxInfo.BackColor = System.Drawing.SystemColors.Highlight;
+            this.groupBoxInfo.Location = new System.Drawing.Point(589, 263);
+            this.groupBoxInfo.Name = "groupBoxInfo";
+            this.groupBoxInfo.Size = new System.Drawing.Size(400, 352);
+            this.groupBoxInfo.TabIndex = 2;
+            this.groupBoxInfo.TabStop = false;
+            this.groupBoxInfo.Text = "信息";
+            // 
+            // groupBoxTime
+            // 
+            this.groupBoxTime.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxTime.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBoxTime.Controls.Add(this.buttonSetTimeReset);
+            this.groupBoxTime.Controls.Add(this.labelColon);
+            this.groupBoxTime.Controls.Add(this.labelSetTimeSecond);
+            this.groupBoxTime.Controls.Add(this.labelSetTimeMinute);
+            this.groupBoxTime.Controls.Add(this.buttonSetTimePause);
+            this.groupBoxTime.Controls.Add(this.buttonSetTimeStart);
+            this.groupBoxTime.Controls.Add(this.LabelSecond);
+            this.groupBoxTime.Controls.Add(this.numericUpDownSetTime);
+            this.groupBoxTime.Controls.Add(this.labelSetTime);
+            this.groupBoxTime.Controls.Add(this.labelSystemTime);
+            this.groupBoxTime.Location = new System.Drawing.Point(589, 3);
+            this.groupBoxTime.Name = "groupBoxTime";
+            this.groupBoxTime.Size = new System.Drawing.Size(400, 250);
+            this.groupBoxTime.TabIndex = 1;
+            this.groupBoxTime.TabStop = false;
+            this.groupBoxTime.Text = "计时工具";
+            // 
+            // buttonSetTimeReset
+            // 
+            this.buttonSetTimeReset.Location = new System.Drawing.Point(331, 209);
+            this.buttonSetTimeReset.Name = "buttonSetTimeReset";
+            this.buttonSetTimeReset.Size = new System.Drawing.Size(44, 33);
+            this.buttonSetTimeReset.TabIndex = 9;
+            this.buttonSetTimeReset.Text = "重置";
+            this.buttonSetTimeReset.UseVisualStyleBackColor = true;
+            this.buttonSetTimeReset.Click += new System.EventHandler(this.buttonSetTimeReset_Click);
+            // 
+            // labelColon
+            // 
+            this.labelColon.BackColor = System.Drawing.Color.Transparent;
+            this.labelColon.Font = new System.Drawing.Font("微软雅黑", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelColon.Location = new System.Drawing.Point(168, 88);
+            this.labelColon.Name = "labelColon";
+            this.labelColon.Size = new System.Drawing.Size(48, 75);
+            this.labelColon.TabIndex = 8;
+            this.labelColon.Text = ":";
+            // 
+            // labelSetTimeSecond
+            // 
+            this.labelSetTimeSecond.AutoSize = true;
+            this.labelSetTimeSecond.BackColor = System.Drawing.Color.Transparent;
+            this.labelSetTimeSecond.Font = new System.Drawing.Font("微软雅黑", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelSetTimeSecond.Location = new System.Drawing.Point(191, 62);
+            this.labelSetTimeSecond.Name = "labelSetTimeSecond";
+            this.labelSetTimeSecond.Size = new System.Drawing.Size(173, 128);
+            this.labelSetTimeSecond.TabIndex = 7;
+            this.labelSetTimeSecond.Text = "00";
+            // 
+            // labelSetTimeMinute
+            // 
+            this.labelSetTimeMinute.Font = new System.Drawing.Font("微软雅黑", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelSetTimeMinute.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.labelSetTimeMinute.Location = new System.Drawing.Point(28, 60);
+            this.labelSetTimeMinute.Name = "labelSetTimeMinute";
+            this.labelSetTimeMinute.Size = new System.Drawing.Size(173, 128);
+            this.labelSetTimeMinute.TabIndex = 6;
+            this.labelSetTimeMinute.Text = "0";
+            this.labelSetTimeMinute.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // buttonSetTimePause
+            // 
+            this.buttonSetTimePause.Location = new System.Drawing.Point(281, 209);
+            this.buttonSetTimePause.Name = "buttonSetTimePause";
+            this.buttonSetTimePause.Size = new System.Drawing.Size(44, 33);
+            this.buttonSetTimePause.TabIndex = 5;
+            this.buttonSetTimePause.Text = "暂停";
+            this.buttonSetTimePause.UseVisualStyleBackColor = true;
+            this.buttonSetTimePause.Click += new System.EventHandler(this.buttonSetTimePause_Click);
+            // 
+            // buttonSetTimeStart
+            // 
+            this.buttonSetTimeStart.Location = new System.Drawing.Point(229, 209);
+            this.buttonSetTimeStart.Name = "buttonSetTimeStart";
+            this.buttonSetTimeStart.Size = new System.Drawing.Size(46, 33);
+            this.buttonSetTimeStart.TabIndex = 4;
+            this.buttonSetTimeStart.Text = "开始";
+            this.buttonSetTimeStart.UseVisualStyleBackColor = true;
+            this.buttonSetTimeStart.Click += new System.EventHandler(this.buttonSetTimeStart_Click);
+            // 
+            // LabelSecond
+            // 
+            this.LabelSecond.AutoSize = true;
+            this.LabelSecond.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold);
+            this.LabelSecond.Location = new System.Drawing.Point(192, 211);
+            this.LabelSecond.Name = "LabelSecond";
+            this.LabelSecond.Size = new System.Drawing.Size(31, 26);
+            this.LabelSecond.TabIndex = 3;
+            this.LabelSecond.Text = "秒";
+            // 
+            // numericUpDownSetTime
+            // 
+            this.numericUpDownSetTime.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold);
+            this.numericUpDownSetTime.Location = new System.Drawing.Point(117, 209);
+            this.numericUpDownSetTime.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.numericUpDownSetTime.Name = "numericUpDownSetTime";
+            this.numericUpDownSetTime.Size = new System.Drawing.Size(69, 33);
+            this.numericUpDownSetTime.TabIndex = 2;
+            // 
+            // labelSetTime
+            // 
+            this.labelSetTime.AutoSize = true;
+            this.labelSetTime.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelSetTime.Location = new System.Drawing.Point(20, 211);
+            this.labelSetTime.Name = "labelSetTime";
+            this.labelSetTime.Size = new System.Drawing.Size(107, 26);
+            this.labelSetTime.TabIndex = 1;
+            this.labelSetTime.Text = "设定时间：";
+            // 
+            // labelSystemTime
+            // 
+            this.labelSystemTime.AutoSize = true;
+            this.labelSystemTime.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelSystemTime.Location = new System.Drawing.Point(18, 32);
+            this.labelSystemTime.Name = "labelSystemTime";
+            this.labelSystemTime.Size = new System.Drawing.Size(117, 28);
+            this.labelSystemTime.TabIndex = 0;
+            this.labelSystemTime.Text = "系统时间：";
+            // 
+            // groupBoxRecord
+            // 
+            this.groupBoxRecord.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.groupBoxRecord.Location = new System.Drawing.Point(3, 3);
+            this.groupBoxRecord.Name = "groupBoxRecord";
+            this.groupBoxRecord.Size = new System.Drawing.Size(580, 612);
+            this.groupBoxRecord.TabIndex = 0;
+            this.groupBoxRecord.TabStop = false;
+            this.groupBoxRecord.Text = "会议记录";
+            // 
+            // timerSetTime
+            // 
+            this.timerSetTime.Interval = 1000;
+            // 
+            // AboutToolStripMenuItem
+            // 
+            this.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem";
+            this.AboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.AboutToolStripMenuItem.Text = "关于";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1008, 730);
+            this.Controls.Add(this.panelMain);
+            this.Controls.Add(this.menuStripMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "模拟联合国主席团工具";
+            this.menuStripMain.ResumeLayout(false);
+            this.menuStripMain.PerformLayout();
+            this.panelMain.ResumeLayout(false);
+            this.groupBoxTime.ResumeLayout(false);
+            this.groupBoxTime.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSetTime)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStripMain;
+        private System.Windows.Forms.ToolStripMenuItem SystemToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ActionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem OtherToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem HelpToolStripMenuItem;
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.GroupBox groupBoxRecord;
+        private System.Windows.Forms.GroupBox groupBoxTime;
+        private System.Windows.Forms.GroupBox groupBoxInfo;
+        private System.Windows.Forms.Label labelSystemTime;
+        private System.Windows.Forms.Timer timerSystemTime;
+        private System.Windows.Forms.Label labelSetTime;
+        private System.Windows.Forms.Label LabelSecond;
+        private System.Windows.Forms.NumericUpDown numericUpDownSetTime;
+        private System.Windows.Forms.Button buttonSetTimePause;
+        private System.Windows.Forms.Button buttonSetTimeStart;
+        private System.Windows.Forms.Label labelSetTimeMinute;
+        private System.Windows.Forms.Timer timerSetTime;
+        private System.Windows.Forms.Label labelColon;
+        private System.Windows.Forms.Label labelSetTimeSecond;
+        private System.Windows.Forms.Button buttonSetTimeReset;
+        private System.Windows.Forms.ToolStripMenuItem AboutToolStripMenuItem;
+    }
+}
+
---
+++ MUN_ChairTools/MUN_ChairTools/MainForm.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MUN_ChairTools
+{
+    public partial class MainForm : Form
+    {
+        private int minutesTemp;
+
+        private int secondsTemp;
+
+        private bool pauseOrContinue = true; // false表示是暂停按钮， true表示继续按钮
+
+        public MainForm()
+        {
+            InitializeComponent();
+            this.timerSystemTime.Enabled = true;
+            this.timerSystemTime.Start();
+            this.timerSystemTime.Tick += new EventHandler(timerSystemTime_Tick);
+
+            this.timerSetTime.Enabled = true;
+            this.timerSetTime.Interval = 1000;
+        }
+
+        void timerSetTime_Tick(object sender, EventArgs e)
+        {
+            
+            if (minutesTemp >= 0)
+            {
+                secondsTemp--;
+                if (secondsTemp == 0)
+                {
+                    minutesTemp--;
+                    this.labelSetTimeMinute.Text = minutesTemp.ToString();
+                    secondsTemp = 59;
+                }
+                labelSetTimeSecond.Text = secondsTemp.ToString();
+            }
+
+            if (minutesTemp == 0 && secondsTemp <= 20)
+            {
+                this.labelSetTimeMinute.ForeColor = Color.Red;
+                this.labelColon.ForeColor = Color.Red;
+                this.labelSetTimeSecond.ForeColor = Color.Red;
+            }
+
+            if (minutesTemp == 0 && secondsTemp == 0)
+            {
+                this.timerSetTime.Stop();
+            }
+        }
+
+        void timerSystemTime_Tick(object sender, EventArgs e)
+        {
+            this.labelSystemTime.Text = "系统时间： " + DateTime.Now.ToString();
+        }
+
+        private void panelMain_SizeChanged(object sender, EventArgs e)
+        {
+            this.groupBoxRecord.Size = new Size(this.panelMain.Size.Width - this.groupBoxTime.Size.Width, this.groupBoxInfo.Size.Height + this.groupBoxTime.Size.Height + 10);
+            this.groupBoxInfo.Location = new Point(this.groupBoxRecord.Size.Width + 6, this.groupBoxTime.Size.Height + 3 + 10);
+        }
+
+        private void buttonSetTimeStart_Click(object sender, EventArgs e)
+        {
+            //开始计时
+            int setTimeSecond = 0;
+            this.secondsTemp = 0;
+            this.minutesTemp = 0;
+            this.labelSetTimeMinute.ForeColor = Color.Black;
+            this.labelColon.ForeColor = Color.Black;
+            this.labelSetTimeSecond.ForeColor = Color.Black;
+
+            if (Int32.TryParse(this.numericUpDownSetTime.Value.ToString(), out setTimeSecond))
+            {
+                //首先把秒转换成 分：秒的形式
+                //Console.WriteLine(this.numericUpDownSetTime.Value.ToString());
+                if (setTimeSecond == 0)
+                {
+                    return;
+                }
+
+                minutesTemp = setTimeSecond / 60;
+                secondsTemp = setTimeSecond - (minutesTemp * 60);
+
+                this.labelSetTimeMinute.Text = minutesTemp.ToString();
+                this.labelSetTimeSecond.Text = secondsTemp.ToString();
+
+                this.timerSetTime.Enabled = true;
+                this.timerSetTime.Start();
+
+                this.timerSetTime.Tick+=new EventHandler(timerSetTime_Tick);
+
+            }
+            else
+            {
+                MessageBox.Show("请输入正确信息！");
+            }
+        }
+
+        private void buttonSetTimePause_Click(object sender, EventArgs e)
+        {
+            //this.timerSetTime.Enabled = false;
+
+            if (pauseOrContinue)
+            {
+                this.pauseOrContinue = !this.pauseOrContinue;
+                this.buttonSetTimePause.Text = "继续";
+                this.timerSetTime.Enabled = false;
+            }
+            else
+            {
+                this.pauseOrContinue = !this.pauseOrContinue;
+                this.buttonSetTimePause.Text = "暂停";
+                this.timerSetTime.Enabled = true;
+            }
+        }
+
+        private void buttonSetTimeReset_Click(object sender, EventArgs e)
+        {
+            this.labelSetTimeSecond.Text = "00";
+            this.labelSetTimeMinute.Text = "0";
+            this.timerSetTime.Enabled = false;
+            this.timerSetTime.Stop();
+            
+            this.numericUpDownSetTime.ResetText();
+
+        }
+
+    }
+}
